1aeeb7725a2ac04c25b84e566a1b2246feabd647
Eddie Zhang <eyzhang@andrew.cmu.edu>
2024-03-21 18:46:49 -0400

    rename from ledge to game class

--- poker/models.py
+++ poker/models.py
@@ -8,15 +8,15 @@ class Profile(models.Model):
     content_type = models.CharField(blank=True, max_length=50)
     wallet = models.DecimalField(max_digits = 6, decimal_places = 2)
     
-class Leger(models.Model):
+class Game(models.Model):
     pot = models.DecimalField(max_digits=10, decimal_places=2)
-    table_num = models.ForeignKey(User, on_delete=models.CASCADE, related_name='table_num')
+    table_num = models.ForeignKey(User, on_delete=models.PROTECT, related_name='table_num')
     small_blind = models.OneToOneField(User, on_delete=models.PROTECT, related_name="small_blind")
     big_blind = models.OneToOneField(User, on_delete=models.PROTECT, related_name="big_blind")
 
 class Participant(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='participant')
-    leger = models.ForeignKey(Leger, on_delete=models.PROTECT, related_name='table_num')
+    leger = models.ForeignKey(Game, on_delete=models.PROTECT, related_name='table_num')
     money_in_hand = models.DecimalField(max_digits=10, decimal_places=2, help_text="Money this user has in hand for this game")
     first_card = models.CharField(max_length=30)
     second_card = models.CharField(max_length=30)
