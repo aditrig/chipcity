bb8e23b96e24904ab754e1c30c950b8753a28d00
Eddie Zhang <eyzhang@andrew.cmu.edu>
2024-03-21 12:42:00 -0400

    leger model and forms

--- poker/models.py
+++ poker/models.py
@@ -6,5 +6,12 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT, related_name="profile")
     picture = models.FileField(blank=True)
     content_type = models.CharField(blank=True, max_length=50)
+    wallet = models.DecimalField(max_digits = 6, decimal_places = 2)
     
+class Leger(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name="profile")
+    pot = models.DecimalField(max_digits=10, decimal_places=2)
+    table_num = models.ForeignKey(User, on_delete=models.CASCADE, related_name='table_num')
+    money_in_hand = models.DecimalField(max_digits=6, decimal_places=2)
+
 
