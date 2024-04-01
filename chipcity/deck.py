8c4be87757100a78f4b0cbb88db03d5b8c660eb0
Eddie Zhang <eyzhang@andrew.cmu.edu>
2024-04-01 17:34:56 -0400

    made game around and round action

--- chipcity/deck.py
+++ chipcity/deck.py
@@ -43,8 +43,11 @@ class Deck:
 
         return list(Deck._FULL_DECK)
 
-class Round_Action:
-    def start_new_round(game_id):
+class Game_Action:
+    def __init__(self):
+        self.round = Game.curr_round
+
+    def start_new_game(self, game_id):
         game = Game.objects.get(id=game_id)
         
         # Reset pot and highest bet for the round
@@ -57,12 +60,22 @@ class Round_Action:
             hand = Hand.objects.get_or_create(game=game, player=player)
             hand.card_left, hand.card_right = deck.draw(2)  # Assuming draw returns two card objects
             hand.save()
+
+        game.flop1 = deck.draw()
+        game.flop2 = deck.draw()
+        game.flop3 = deck.draw()
+        game.turn = deck.draw()
+        game.river = deck.draw()
         
         # Set the first player as the current player
         round_instance.current_player = game.players.first()
         round_instance.save()
 
-    def player_action(game_id, player_id, action_type, bet_amount=0):
+    def round_action(self, game_id, player_id, action_type):
+        self.player_action(game_id, player_id, action_type, bet_amount=0)
+        self.round += 1
+
+    def player_action(self, game_id, player_id, action_type, bet_amount=0):
         game = Game.objects.get(id=game_id)
         player = Player.objects.get(id=player_id)
         current_round = game.round_game.last()
