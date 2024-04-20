b70ca0c7be5056b5317ac092b0ddb36c91ed7dff
Eddie Zhang <eyzhang@andrew.cmu.edu>
2024-04-20 01:36:41 -0400

    added allowed hosts

--- webapps/settings.py
+++ webapps/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = CONFIG.get("Django", "Secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["18.218.127.71"]
 
 # Application definition
 
